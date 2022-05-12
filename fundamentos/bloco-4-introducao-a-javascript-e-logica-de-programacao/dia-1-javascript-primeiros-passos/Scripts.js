8fca82f365fbdb961f5921ffee2178f369811295
Luídi <luidihot@gmail.com>
2022-05-12 18:57:13 -0300

    5 Primeiras operações aritméticas

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Scripts.js
@@ -0,0 +1,17 @@
+const a = 13
+const b = 13
+
+let Adição = a + b
+console.log(Adição);
+
+let Subtração = a - b
+console.log(Subtração);
+
+let Multiplicação = a * b
+console.log(Multiplicação);
+
+let Divisão = a / b
+console.log(Divisão);
+
+let Modulo = a % b
+console.log(Modulo);
