58fbf1c9d1f2025570705ae006e146152613bc15
Luídi <luidihot@gmail.com>
2022-05-13 16:00:52 -0300

    Exercício 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Scripts.js
@@ -2,13 +2,19 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let imprima of numbers){
-    console.log(imprima);
-}   console.log();
+// for(let imprima of numbers){
+//     console.log(imprima);
+// }   console.log()
 
-// Parte 2
+// // Parte 2
 
-for(let num of numbers){
-    let soma = num + num 
-    console.log(soma);
-}   console.log();
+// for(let num of numbers){
+//     let soma = num + num 
+//     console.log(soma);
+// }   console.log();
+
+// Parte 3
+let total = 0 
+for(let valores of numbers){
+    total = total + valores
+}console.log(total / numbers.length);
