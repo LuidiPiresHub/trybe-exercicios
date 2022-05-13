56f274b8a718a04cecf73fe8e6f93894bf009e0c
Luídi <luidihot@gmail.com>
2022-05-13 00:45:39 -0300

    Fim

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Scripts.js
@@ -152,3 +152,49 @@ if(num1 % 2 === 1|| num2 % 2 === 1 || num3 % 2 === 1){
 } else {
     console.log(false);
 }
+
+// Parte 10
+
+const custo = 3000
+const valorVenda = 5000
+
+if(custo > 0 && valorVenda > 0){
+    let valorCustoTotal = custo * 1.2;
+    let lucro = (valorVenda - valorCustoTotal) * 1000;  
+    console.log(lucro);  
+} else {
+    console.log("Error. valor incorreto!");
+}
+
+// Parte 11
+
+let aliquotINSS;
+let aliquotIR;
+
+const grossSalary = 2000.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+}
+
+const baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
