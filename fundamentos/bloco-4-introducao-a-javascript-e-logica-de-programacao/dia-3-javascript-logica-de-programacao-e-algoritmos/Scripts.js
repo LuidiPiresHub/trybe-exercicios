96e60225711dde971180a7f75296800a7d61859a
Luídi <luidihot@gmail.com>
2022-05-16 16:25:53 -0300

    Exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Scripts.js
@@ -1,3 +1,5 @@
+// Parte 1
+
 let num = 10
 
 let fat = 1
@@ -5,3 +7,11 @@ for(let i = 1; i <= num; i += 1){
   fat = fat * i
 }
 console.log(fat)
+
+// Parte 2
+
+let word = 'Tryber';
+
+for(let i = word.length - 1; i > -1; i -= 1){
+  console.log(word[i])
+}
