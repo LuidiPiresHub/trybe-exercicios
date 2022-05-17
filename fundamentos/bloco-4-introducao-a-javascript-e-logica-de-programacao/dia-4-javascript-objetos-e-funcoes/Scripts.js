104d6a87245ce1d985bae04a867c61a6dd569bac
Luídi <luidihot@gmail.com>
2022-05-17 18:36:37 -0300

    Exercício 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
@@ -6,14 +6,24 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
   console.log('Bem-vinda, ' + info.personagem)
+  console.log();
 
 //   Parte 2
 
 info['recorrente'] = 'sim'
 console.log(info);
+console.log();
 
 // Parte 3
 
 for (let key in info) {
     console.log(key);
 }
+console.log();
+
+// Parte 4
+
+for (let val in info) {
+    console.log(info[val]);
+}
+console.log();
