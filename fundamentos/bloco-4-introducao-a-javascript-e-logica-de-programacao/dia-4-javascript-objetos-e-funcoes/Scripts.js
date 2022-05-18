d6a7c93a9a4adef3ef51f98a13743d9e52faa717
Luídi <luidihot@gmail.com>
2022-05-17 22:19:08 -0300

    Exercício 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
@@ -60,9 +60,19 @@ let leitor = {
         autor: 'Daniela Kopsch',
         editora: 'Tordesilhas',
       },
+      {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      }
     ],
-  };
+ };
 
   console.log(leitor.nome, leitor.sobrenome);
   console.log("O livro favorito de " + leitor.nome + ' ' + leitor.sobrenome + "se chama: " + leitor.livrosFavoritos[0].titulo);
+  console.log();
+
+  // Parte 7
+
+  console.log(leitor.nome + ' ' + 'tem' + ' ' + leitor.livrosFavoritos.length + ' ' + 'livros favoritos');
   console.log();
