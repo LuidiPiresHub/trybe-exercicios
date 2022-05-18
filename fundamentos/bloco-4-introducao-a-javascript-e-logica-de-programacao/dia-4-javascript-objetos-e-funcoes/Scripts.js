1c3d600ef1998bf588cbae731c2197e39bf39354
Luídi <luidihot@gmail.com>
2022-05-17 21:39:18 -0300

    Exercício 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
@@ -47,3 +47,22 @@ for (let val in info) {
       console.log(info[val] + ' e ' + info2[val]);
     }
   }
+
+//   Parte 6
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  console.log(leitor.nome, leitor.sobrenome);
+  console.log("O livro favorito de " + leitor.nome + ' ' + leitor.sobrenome + "se chama: " + leitor.livrosFavoritos[0].titulo);
+  console.log();
