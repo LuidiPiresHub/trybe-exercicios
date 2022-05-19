aef0308c3ad87b8f0aad4a14a62b6c5a421b6155
Luídi <luidihot@gmail.com>
2022-05-19 18:21:56 -0300

    Exercício 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Scripts.js
@@ -1,1 +1,4 @@
-console.log(document.getElementById("future").innerText = "Daqui a 2 anos se Deus quiser já estarei trabalho e poderei ter meu primeiro carrinho!")
+document.getElementById("future").innerText = "Daqui a 2 anos se Deus quiser já estarei trabalho e poderei ter meu primeiro carrinho!"
+
+let backgroundColor = document.getElementsByClassName('main-content')[0]
+backgroundColor.style.backgroundColor = 'rgb(76,164,109)'
