6bb1b6e5e6da25c5a924be72f8ce6ea1a3da4ada
Luídi <luidihot@gmail.com>
2022-05-20 14:44:25 -0300

    Transformando em funções, porém incompletas

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Scripts.js
@@ -1,9 +1,28 @@
-document.getElementById("future").innerText = "Daqui a 2 anos se Deus quiser já estarei trabalho e poderei ter meu primeiro carrinho!"
+function euMeVejo() {
+    document.getElementById("future").innerText = "Daqui a 2 anos se Deus quiser já estarei trabalho e poderei ter meu primeiro carrinho!"
+}
+console.log(euMeVejo());
 
-let backgroundColor = document.getElementsByClassName('main-content')[0]
-backgroundColor.style.backgroundColor = 'rgb(76,164,109)'
+function corFundo() {
+    let backgroundColor = document.getElementsByClassName('main-content')[0]
+    backgroundColor.style.backgroundColor = 'rgb(76,164,109)'
+}
+console.log(corFundo());
 
-let backgroundColor2 = document.getElementsByClassName('center-content')[0]
-backgroundColor2.style.backgroundColor = 'white'
+function corCentro() {
+    let backgroundColor2 = document.getElementsByClassName('center-content')[0]
+    backgroundColor2.style.backgroundColor = 'white'
+}
+console.log(corCentro());
 
-document.getElementsByClassName('title')[0].innerText = 'JavaScripit'
+function corrige() {
+    document.getElementsByClassName('title')[0].innerText = 'JavaScripit'
+}
+console.log(corrige());
+
+
+function paragraphUpperCase() {
+    let paragraph = document.getElementsByTagName('p')[0];
+    paragraph.innerHTML = paragraph.innerHTML.toUpperCase();
+  }
+  console.log(paragraphUpperCase());
