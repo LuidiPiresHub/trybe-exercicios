9763a80ec08979766d51526fcc280c8dd432147e
Luídi <luidihot@gmail.com>
2022-05-22 20:36:37 -0300

    Exercício 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Scripts.js
@@ -184,3 +184,19 @@ function somatorio(n) {
   return soma
 }
 console.log(somatorio(5));
+console.log();
+
+// Parte 7
+
+function verificaFimPalavra(palavra, fimPalavra) {
+  palavra = palavra.split('');
+  fimPalavra = fimPalavra.split('');
+  controle = true;
+  for (let index = 0; index < fimPalavra.length; index += 1) {
+    if (palavra[palavra.length - fimPalavra.length + index] != fimPalavra[index]) {
+      controle = false;
+    }
+  }
+  return controle;
+}
+console.log(verificaFimPalavra('trybe', 'be'))
