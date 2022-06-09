771b28cd912404ada174614bdf687de8e55bf1e7
Luídi <luidihot@gmail.com>
2022-06-08 22:29:28 -0300

    Exercício 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,4 +1,5 @@
 // Parte 1
+// Exercício 1
 
 const testingScope = (escopo) => {
     if (escopo === true) {
@@ -13,10 +14,27 @@ const testingScope = (escopo) => {
 
 testingScope(true);
 
-// Parte 2
+// Exercício 2
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
 console.log(`Os Numeros ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
+
+// Parte 2
+// Exercício 1
+
+const fatorial = (number) => {
+    let resultado = number
+    for (let i = number -1; i > 0; i -= 1) {
+        resultado *= i
+    }
+    return resultado
+}
+console.log(fatorial(4));
+
+// OU
+
+const factorial = (number) => number > 1 ? number * factorial(number - 1) : 1;
+console.log(factorial(4));
