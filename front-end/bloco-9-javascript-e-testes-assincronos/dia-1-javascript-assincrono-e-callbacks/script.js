ee98df6c842359d2faf9b01b370f7941eb3e9fe6
Luídi <luidihot@gmail.com>
2022-07-06 20:41:09 -0300

    Exercício 4

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -34,30 +34,30 @@
 // Exercicio 2
 
 const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+    `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
 const mars = {
-  name: "Mars",
-  distanceFromSun: {
-    value: 227900000,
-    measurementUnit: "kilometers",
-  },
+    name: "Mars",
+    distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+    },
 };
 
 const venus = {
-  name: "Venus",
-  distanceFromSun: {
-    value: 108200000,
-    measurementUnit: "kilometers",
-  },
+    name: "Venus",
+    distanceFromSun: {
+        value: 108200000,
+        measurementUnit: "kilometers",
+    },
 };
 
 const jupiter = {
-  name: "Jupiter",
-  distanceFromSun: {
-    value: 778500000,
-    measurementUnit: "kilometers",
-  },
+    name: "Jupiter",
+    distanceFromSun: {
+        value: 778500000,
+        measurementUnit: "kilometers",
+    },
 };
 
 console.log(planetDistanceFromSun(mars)); // A
@@ -78,3 +78,26 @@ const getPlanet = () => {
 };
 
 setTimeout(() => getPlanet(), 4000)  // imprime Marte depois de 4 segundos
+
+// Exercicio 4
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+    const maxTemperature = 58;
+    return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const sendMarsTemperature = () => {
+    setTimeout(() => {
+        const temp = getMarsTemperature();
+        return console.log(`Mars temperature is: ${temp} degree Celsius`);
+    }, 5000)
+};
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// Exercicio 5
+
