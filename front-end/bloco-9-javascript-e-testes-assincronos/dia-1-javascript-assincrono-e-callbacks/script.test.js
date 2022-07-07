751b7975989e566a256dab341652be4315b18241
Luídi <luidihot@gmail.com>
2022-07-06 21:45:29 -0300

    Exercicio 9

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.test.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.test.js
@@ -1,3 +1,6 @@
+const { getPokemonDetails } = require("./script.js");
+jest.setTimeout(10000)
+
 const uppercase = (str, callback) => {
   setTimeout(() => {
     callback(str.toUpperCase());
@@ -14,5 +17,30 @@ describe('Exercício 7', () => {
         done(error);
       }
     });
+  });
+});
+
+
+describe("A função getPokemonDetails", () => {
+  it("retorna erro quando procuramos um pokemon que não existe no banco de dados", (done) => {
+    const expectedError = new Error('Não temos esse pokémon para você :(');
+
+    function callback(error, result) {
+      expect(error).toEqual(expectedError);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Pikachu', callback);
+  });
+
+  it("retorna um pokemon que existe no banco de dados", (done) => {
+    const expectedString = 'Olá, seu pokemon é o Charmander, o tipo dele é Fire e a habilidade principal dele é Ember';
+
+    function callback(err, result) {
+      expect(result).toBe(expectedString);
+      done();
+    }
+
+    getPokemonDetails((pokemon) => pokemon.name === 'Charmander', callback);
   });
 });
