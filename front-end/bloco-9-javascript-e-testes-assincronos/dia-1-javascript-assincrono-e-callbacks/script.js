4e330f2daea8c18439d7b7a28f726fa3b9a17a71
Luídi <luidihot@gmail.com>
2022-07-06 21:37:48 -0300

    Exercício 8

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -157,3 +157,51 @@ const sendMarsTemperature3 = (onSuccess, onError) => {
 
 sendMarsTemperature3(temperatureInFahrenheit3, handleError);
 sendMarsTemperature3(greet3, handleError);
+
+// Exercicio 9
+
+const pokemons = [
+    {
+      name: 'Bulbasaur',
+      type: 'Grass',
+      ability: 'Razor Leaf',
+    },
+    {
+      name: 'Charmander',
+      type: 'Fire',
+      ability: 'Ember',
+    },
+    {
+      name: 'Squirtle',
+      type: 'Water',
+      ability: 'Water Gun',
+    },
+  ];
+  
+  function getPokemonDetails(filter, callback) {
+    setTimeout(() => {
+      if (pokemons.find(filter) === undefined) {
+        return callback(new Error('Não temos esse pokemon para você :('), null);
+      }
+      const pokemon = pokemons.find(filter);
+  
+      const { name, type, ability } = pokemon;
+  
+      const messageFromProfOak = `Olá, seu pokemon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+  
+      callback(null, messageFromProfOak);
+    }, 2000);
+  }
+  
+  getPokemonDetails(
+    (pokemon) => pokemon.name === 'Bulbasaur',
+    (error, message) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(message);
+      }
+    }
+  );
+  
+  module.exports = { getPokemonDetails };
